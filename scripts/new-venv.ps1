3e30dfe4d3fad16b99f06458bcaad528d9fefaad
Stuart Williams <spookdejur@hotmail.com>
2025-11-30 12:00:26 -0800

    folders,3

--- scripts/new-venv.ps1
+++ scripts/new-venv.ps1
@@ -3,19 +3,21 @@
     if exists, force creation of a new clean one
 #>
 
+[string]$venv_dir = '.venv'
+
 . $PSScriptRoot/Find-GitRepositoryRoot.ps1
 
 $gitR = Find-GitRepositoryRoot -FilePath $PSScriptRoot;
 
 if ($null -ne $gitR) {
 
-    $venvR = Join-Path -Path $gitR -ChildPath 'venv' 
+    $venvR = Join-Path -Path $gitR -ChildPath  $venv_dir
     if (Test-Path -Path $venvR -PathType Container) {
         Remove-Item -Path $venvR -Recurse -Force
     }
 
     Push-Location $gitR
-    python -m venv venv
-    .\venv\Scripts\Activate.ps1
+    python -m venv $venv_dir
+    . ".\${venv_dir}\Scripts\Activate.ps1"
     Pop-Location
 }
