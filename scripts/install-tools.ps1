2743cec384b750eea52dcd3a9ede796b56f65d3f
Stuart Williams <spookdejur@hotmail.com>
2025-12-03 14:22:01 -0800

    folders,b

--- scripts/install-tools.ps1
+++ scripts/install-tools.ps1
@@ -5,7 +5,15 @@
 python.exe -m pip install --upgrade pip
 pip install uv
 
+# Force update
+uv self update
+
 # Use UV to Install Tools
 uv tool install black
 uv tool install isort
 uv tool install ruff
+uv tool install pyright
+uv tool install pytest
+
+# This should be last
+uv tool update-shell
