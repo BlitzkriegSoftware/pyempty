4299ad22116ef93bcb5cd213bda9f152dd040a07
Stuart Williams <spookdejur@hotmail.com>
2025-12-07 17:10:19 -0800

    Tools updates, Docker support, 5

--- tests/e2e_tests.py
+++ tests/e2e_tests.py
@@ -1,9 +1,11 @@
 from fastapi.testclient import TestClient
+import pytest
 
-from ..src.main import app
+from ..src.webserver import app
 
 client = TestClient(app)
 
+@pytest.mark.api
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
